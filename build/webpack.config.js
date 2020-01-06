456d9273415d2635a7d55deae4ed54e2fddb4865
Kristen12 <569098752@qq.com>
2020-01-06 18:49:09 +0800

    初始化项目&配置html模版

---
+++ build/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  mode: "development", // 开发模式
+  entry: {
+    main: path.resolve(__dirname, "../src/main.js"), // 入口文件
+    header: path.resolve(__dirname, "../src/head.js") // 多入口文件
+  },
+  output: {
+    filename: "[name].[hash:8].js", // 打包后的文件名称
+    path: path.resolve(__dirname, "../dist") // 打包后的目录
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+      filename: "index.html",
+      chunks: ["main"] // 与入口文件对应的模块名
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/head.html"),
+      filename: "head.html",
+      chunks: ["header"] // 与入口文件对应的模块名
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
