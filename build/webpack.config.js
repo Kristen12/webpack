c9bc66e8af9145d0b4b2edda6d6ba18c12143170
Kristen12 <569098752@qq.com>
2020-01-07 15:54:00 +0800

    引入CSS & 打包媒体资源 & 转译js文件

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development", // 开发模式
   entry: {
-    main: path.resolve(__dirname, "../src/main.js"), // 入口文件
+    main: ["@babel/polyfill", path.resolve(__dirname, "../src/main.js")], // 入口文件  @babel/polyfill编译新API
     header: path.resolve(__dirname, "../src/head.js") // 多入口文件
   },
   output: {
@@ -22,6 +23,105 @@ module.exports = {
       filename: "head.html",
       chunks: ["header"] // 与入口文件对应的模块名
     }),
-    new CleanWebpackPlugin()
-  ]
+    new CleanWebpackPlugin(), // 清除dist
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash:8].css",
+      chunkFilename: "[id].css"
+    }) // css分离（用外链形式引入css）
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader", // 添加css前缀
+            options: {
+              plugins: [require("autoprefixer")]
+            }
+          }
+        ] // 从右向左解析原则
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")]
+            }
+          },
+          "less-loader"
+        ] // 从右向左解析原则
+      },
+
+      {
+        test: /\.(jpe?g|png|gif)$/i, //图片文件
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: "file-loader",
+                options: {
+                  name: "img/[name].[hash:8].[ext]"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: "file-loader",
+                options: {
+                  name: "media/[name].[hash:8].[ext]"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: "file-loader",
+                options: {
+                  name: "fonts/[name].[hash:8].[ext]"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/, //转译js
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import "./assets/index.css";
+import "./assets/index.less";
 console.log("kristen");
+let a = "abc";
+console.log(a);
+var promise = new Promise();
+console.log(promise);
